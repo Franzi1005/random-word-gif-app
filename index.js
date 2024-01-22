7c6776db6f7267e14be0c87955957cdd8cb56d68
Franzi1005 <fschallhorn@gmail.com>
2024-01-22 18:47:42 +0100

    made changes to implemetnt search api

--- index.js
+++ index.js
@@ -1,33 +1,63 @@
 let initialButton = document.querySelector("#initial-button");
 
-initialButton.addEventListener("click", getWord);
+initialButton.addEventListener("click", getWordAsync);
 
-function getWord() {
-  let apiUrl = "https://random-word-api.herokuapp.com/word";
+/* function getWord() {
+  let apiUrlWord = "https://random-word-api.herokuapp.com/word";
   axios
-    .get(apiUrl)
+    .get(apiUrlWord)
     .then(displayWord)
     .catch(console.log("sorry, we didn't find a word"))
     .then(searchGif)
     .catch(console.log("sorry, this request failed"));
-}
+} */
 
 function displayWord(response) {
-  let word = response.data[0];
+  let word = response.data;
   let wordResult = document.querySelector("#word-result");
   wordResult.innerHTML = `Your word is ${word}`;
 }
 
-function searchGif(word) {
+/* function searchGif(word) {
   let apiKey = "4EKhSnBxhdLZ1jXoGXi9SnpsrCeFYIBQ";
   let gifApiUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${word}&rating=g`;
   axios.get(gifApiUrl).then(showGif);
-}
+} */
 
 function showGif(response) {
-  let gif = response.data.data.images.original.url;
+  console.log(response.data.data[0].images.downsized.url);
+  let gif = response.data.data[0].images.downsized.url;
   let gifResult = document.querySelector("#gif-result");
   gifResult.innerHTML = `<img src="${gif}" alt="gif">`;
+}
 
-  //consolelog(gif).;
+// async await
+
+debugger;
+
+async function getWordAsync(event) {
+  event.preventDefault();
+  let responseWord;
+  try {
+    responseWord = await axios.get(
+      "https://random-word-api.herokuapp.com/word"
+    );
+    displayWord(responseWord);
+  } catch (error) {
+    console.log(error);
+    console.error("oops, error! MÖÖP, MÖÖP!");
+  }
+
+  try {
+    let apiKey = "4EKhSnBxhdLZ1jXoGXi9SnpsrCeFYIBQ";
+    let responseGif = await axios.get(
+      `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${responseWord}`
+    );
+    showGif(responseGif);
+  } catch (error) {
+    console.log(error);
+    console.error("oops, error! MÖÖP, MÖÖP, MÖÖP!");
+    let gifResult = document.querySelector("#gif-result");
+    gifResult.innerHTML = `<h2>Shoot, nothing found for your search word! This app sucks!</h2>`;
+  }
 }
